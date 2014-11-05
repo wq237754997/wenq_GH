30cf222d70e188a8c59d1a0dcad4a57693e67d6b
wq237754997 <237754997@qq.com>
2014-11-05 11:04:52 +0800

    20141105

---
+++ JavaScript/j-plug-in/j-plug-in-tpl.js
@@ -0,0 +1,15 @@
+/* 
+// 插件模版
+(function($) {
+    var defaults = {
+
+    };
+
+    $.tab = function(options){
+        var options = $.extend(defaults,options);
+        return this.each(function(){ // 对每个元素进行操作
+
+        });
+    };
+})(jQuery);
+*/
